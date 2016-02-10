7d3dff7e47fe59e5c65ffee3558f795ddb6d0aa4
Jayden Shepard <jaydenshepard@gmail.com>
2016-02-10 15:45:50 -0500

    added 404 page, working on add_category page

--- Vagrant/app/catalog.py
+++ Vagrant/app/catalog.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from flask.ext.bootstrap import Bootstrap
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Categories, Items
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 # Initialises app and extension modules
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 
 
@@ -16,10 +20,32 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+class CategoryForm(Form):
+    name = StringField("What is the category you'd like to add?", validators=[DataRequired()])
+    submit = SubmitField('submit')
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/category/add', methods=['GET', 'POST'])
+def add_category():
+    form = CategoryForm()
+    if form.validate_on_submit():
+        category = session.query(Categories).filter_by(name = form.name.data).first()
+        if category is None:
+            category = Categories(name=form.name.data)
+            session.add(category)
+        else:
+            form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('add_category.html', form=form)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
